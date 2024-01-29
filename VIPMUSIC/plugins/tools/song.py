40bf3c1910cfb13610196b68c38478ce86660124
MoHiT <145571381+mohit828@users.noreply.github.com>
2024-01-29 11:21:23 +0530

    Update song.py

--- VIPMUSIC/plugins/tools/song.py
+++ VIPMUSIC/plugins/tools/song.py
@@ -39,7 +39,7 @@ async def download_media(message: Message, audio: bool = True):
         ]
 
         quality_buttons = [
-            InlineKeyboardButton(option["label"], callback_data=f'{option["itag"]}_{media_link}_{media_title}_{audio}')
+            InlineKeyboardButton(option["label"], callback_data=f'{option["itag"]}_{media_title}')
             for option in quality_options
         ]
 
